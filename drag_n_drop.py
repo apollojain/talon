5e35c98fb45d086c62b0252bdcd87fda7fce6454
Apollo Jain <apollojain@Apollos-MBP.home>
2017-08-06 15:45:26 -0400

    add drag and drop

--- drag_n_drop.py
+++ drag_n_drop.py
@@ -1,107 +1,68 @@
+import sys
 import os
-import stat
-import time
-import wx
-from ObjectListView import ObjectListView, ColumnDefn
- 
-########################################################################
-class MyFileDropTarget(wx.FileDropTarget):
-    """"""
- 
-    #----------------------------------------------------------------------
-    def __init__(self, window):
-        """Constructor"""
-        wx.FileDropTarget.__init__(self)
-        self.window = window
- 
-    #----------------------------------------------------------------------
-    def OnDropFiles(self, x, y, filenames):
-        """
-        When files are dropped, update the display
-        """
-        self.window.updateDisplay(filenames)
- 
-########################################################################
-class FileInfo(object):
-    """"""
- 
-    #----------------------------------------------------------------------
-    def __init__(self, path, date_created, date_modified, size):
-        """Constructor"""
-        self.name = os.path.basename(path)
-        self.path = path
-        self.date_created = date_created
-        self.date_modified = date_modified
-        self.size = size
- 
-########################################################################
-class MainPanel(wx.Panel):
-    """"""
- 
-    #----------------------------------------------------------------------
-    def __init__(self, parent):
-        """Constructor"""
-        wx.Panel.__init__(self, parent=parent)
-        self.file_list = []
- 
-        file_drop_target = MyFileDropTarget(self)
-        self.olv = ObjectListView(self, style=wx.LC_REPORT|wx.SUNKEN_BORDER)
-        self.olv.SetDropTarget(file_drop_target)
-        self.setFiles()
- 
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.olv, 1, wx.EXPAND)
-        self.SetSizer(sizer)
- 
-    #----------------------------------------------------------------------
-    def updateDisplay(self, file_list):
-        """"""
-        for path in file_list:
-            file_stats = os.stat(path)
-            creation_time = time.strftime("%m/%d/%Y %I:%M %p",
-                                          time.localtime(file_stats[stat.ST_CTIME]))
-            modified_time = time.strftime("%m/%d/%Y %I:%M %p",
-                                          time.localtime(file_stats[stat.ST_MTIME]))
-            file_size = file_stats[stat.ST_SIZE]
-            if file_size > 1024:
-                file_size = file_size / 1024.0
-                file_size = "%.2f KB" % file_size
- 
-            self.file_list.append(FileInfo(path,
-                                           creation_time,
-                                           modified_time,
-                                           file_size))
- 
-        self.olv.SetObjects(self.file_list)
- 
-    #----------------------------------------------------------------------
-    def setFiles(self):
-        """"""
-        self.olv.SetColumns([
-            ColumnDefn("Name", "left", 220, "name"),
-            ColumnDefn("Date created", "left", 150, "date_created"),
-            ColumnDefn("Date modified", "left", 150, "date_modified"),
-            ColumnDefn("Size", "left", 100, "size")
-            ])
-        self.olv.SetObjects(self.file_list)
- 
-########################################################################
-class MainFrame(wx.Frame):
-    """"""
- 
-    #----------------------------------------------------------------------
-    def __init__(self):
-        """Constructor"""
-        wx.Frame.__init__(self, None, title="OLV DnD Tutorial", size=(800,600))
-        panel = MainPanel(self)
-        self.Show()
- 
-#----------------------------------------------------------------------
+from PySide import QtGui, QtCore
+
+'''
+virtualenv venv --distribute
+source venv/bin/activate
+'''
+class TestListView(QtGui.QListWidget):
+
+    fileDropped = QtCore.Signal(list)
+
+    def __init__(self, type, parent=None):
+        super(TestListView, self).__init__(parent)
+        self.setAcceptDrops(True)
+        self.setIconSize(QtCore.QSize(72, 72))
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.accept()
+        else:
+            event.ignore()
+
+    def dragMoveEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.setDropAction(QtCore.Qt.CopyAction)
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.setDropAction(QtCore.Qt.CopyAction)
+            event.accept()
+            links = []
+            for url in event.mimeData().urls():
+                links.append(str(url.toLocalFile()))
+            self.fileDropped.emit(links)
+        else:
+            event.ignore()
+
+class MainForm(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        super(MainForm, self).__init__(parent)
+
+        self.view = TestListView(self)
+        self.view.fileDropped.connect(self.pictureDropped)
+        self.setCentralWidget(self.view)
+
+    def pictureDropped(self, l):
+        for url in l:
+            if os.path.exists(url):
+                print(url)                
+                icon = QtGui.QIcon(url)
+                pixmap = icon.pixmap(72, 72)                
+                icon = QtGui.QIcon(pixmap)
+                item = QtGui.QListWidgetItem(url, self.view)
+                item.setIcon(icon)        
+                item.setStatusTip(url)        
+
 def main():
-    """"""
-    app = wx.App(False)
-    frame = MainFrame()
-    app.MainLoop()
- 
-if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    form = MainForm()
+    form.show()
+    app.exec_()
+
+if __name__ == '__main__':
     main()
